10fbea3fa9c7c74e80602c8e3331fa6d2c8e58e5
PatDahl79 <pattaravarat.dahl@chasacademy.se>
2024-05-21 13:10:36 +0200

    ,,
    ,,

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    // Ensure proper MIME type handling
-    mimeTypes: {
-      'application/javascript': ['js']
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
     },
   },
 })
-
 
